286088115d87b3af8e676fc6a4d05d4a594f33b9
Martijn van Welie <martijn.van.welie@gmail.com>
2021-02-22 09:14:34 +0100

    Completed HR parsing

--- src/main/kotlin/com/welie/healthhub/HeartRateMeasurement.kt
+++ src/main/kotlin/com/welie/healthhub/HeartRateMeasurement.kt
@@ -5,15 +5,46 @@ import com.welie.blessed.BluetoothBytesParser.FORMAT_UINT16
 import com.welie.blessed.BluetoothBytesParser.FORMAT_UINT8
 
 data class HeartRateMeasurement(
-    val pulse: Int
+    val pulse: Int,
+    val energyExpended: Int?,
+    val rrIntervals: IntArray,
+    val sensorContactStatus: SensorContactFeature
 ) {
     companion object {
         fun fromBytes(value: ByteArray): HeartRateMeasurement {
             val parser = BluetoothBytesParser(value)
             val flags = parser.getIntValue(FORMAT_UINT8)
             val pulse = if (flags and 0x01 == 0) parser.getIntValue(FORMAT_UINT8) else parser.getIntValue(FORMAT_UINT16)
+            val sensorContactStatusFlag = flags and 0x06 shr 1
+            val energyExpenditurePresent = flags and 0x08 > 0
+            val rrIntervalPresent = flags and 0x10 > 0
 
-            return HeartRateMeasurement(pulse = pulse)
+            val sensorContactStatus = when (sensorContactStatusFlag) {
+                0, 1 ->  //Sensor Contact feature is not supported in the current connection
+                    SensorContactFeature.NotSupported
+                2 ->  //Sensor Contact feature is supported, but contact is not detected
+                    SensorContactFeature.SupportedNoContact
+                3 ->  //Sensor Contact feature is supported and contact is detected
+                    SensorContactFeature.SupportedAndContact
+                else -> SensorContactFeature.NotSupported
+            }
+
+            val energyExpended = if (energyExpenditurePresent) parser.getIntValue(FORMAT_UINT16) else null
+
+            val rrArray = ArrayList<Int>()
+            if (rrIntervalPresent) {
+                while (parser.offset < value.size) {
+                    val rrInterval = parser.getIntValue(FORMAT_UINT16)
+                    rrArray.add((rrInterval.toDouble() / 1024.0 * 1000.0).toInt())
+                }
+            }
+
+            return HeartRateMeasurement(
+                pulse = requireNotNull(pulse),
+                energyExpended = energyExpended,
+                sensorContactStatus = sensorContactStatus,
+                rrIntervals = rrArray.toIntArray()
+            )
         }
     }
 }
